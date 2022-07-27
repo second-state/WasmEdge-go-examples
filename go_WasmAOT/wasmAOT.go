693067eb097c6c51d71af1f55f803e795f8f1825
YiYing He <yiying@secondstate.io>
2022-07-27 19:34:18 +0800

    [Examples] Update the AOT compiler example to turn on the proposals.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- go_WasmAOT/wasmAOT.go
+++ go_WasmAOT/wasmAOT.go
@@ -9,21 +9,25 @@ import (
 
 func main() {
 	fmt.Println("Go: Args:", os.Args)
-	/// Expected Args[0]: program name (./wasmAOT)
-	/// Expected Args[1]: wasm file path to compile
-	/// Expected Args[2]: output compiled-wasm path
+	// Expected Args[0]: program name (./wasmAOT)
+	// Expected Args[1]: wasm file path to compile
+	// Expected Args[2]: output compiled-wasm path
 
-	/// Set not to print debug info
+	// Set not to print debug info
 	wasmedge.SetLogErrorLevel()
 
-	/// Create Compiler
-	var compiler = wasmedge.NewCompiler()
+	// Create Configure
+	conf := wasmedge.NewConfigure(wasmedge.THREADS, wasmedge.EXTENDED_CONST, wasmedge.TAIL_CALL, wasmedge.MULTI_MEMORIES)
 
-	/// Compile WASM AOT
+	// Create Compiler
+	compiler := wasmedge.NewCompilerWithConfig(conf)
+
+	// Compile WASM AOT
 	err := compiler.Compile(os.Args[1], os.Args[2])
 	if err != nil {
 		fmt.Println("Go: Compile WASM to AOT mode Failed!!")
 	}
 
+	conf.Release()
 	compiler.Release()
 }
